77c6ecf558c2b1ec0dae0b6ada6f24db6d00ee03
Samantha Vivonia <snvivonia@gmail.com>
2021-11-23 05:22:43 +0000

    Completed express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+var nextId = 1;
+var grades = {};
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('Express server listening on port 3000');
+});
+
+app.get('/api/grades', (req, res) => {
+  var gradesArray = [];
+  for (var property in grades) {
+    gradesArray.push(grades[property]);
+  }
+  res.json(gradesArray);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  res.status(201);
+  var newObj = req.body;
+  newObj.id = nextId;
+  grades[nextId] = newObj;
+  nextId++;
+  res.json(grades);
+});
